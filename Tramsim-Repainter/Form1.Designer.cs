6dc2105297d4aa7d27031c7d84623ffe815dd2bd
Liebeg <leo.cappellaro@gmail.com>
2024-05-26 16:00:36 +0200

    Add files via upload

---
+++ Tramsim-Repainter/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Tramsim_Repainter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(111, 32);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(231, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Confirm Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "Repainter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tramsim-Repainter/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tramsim_Repainter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string pfad = AppDomain.CurrentDomain.BaseDirectory;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string RepaintPath;
+            RepaintPath= txtPath.Text;
+
+            // Dateipfad für die zu erstellende Datei
+            string dateiPfad = Path.Combine(pfad, "path.txt");
+
+            // Schreiben des Pfads in die Datei
+            File.WriteAllText(dateiPfad, RepaintPath);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Creates the intial path file
+            string dateiPfad = Path.Combine(pfad, "path.txt");
+                       
+            if (File.Exists(dateiPfad))
+            {
+                txtPath.Text = File.ReadAllText(dateiPfad);
+            } else
+            {
+                File.WriteAllText(dateiPfad, "");
+            }
+
+        }
+    }
+}
